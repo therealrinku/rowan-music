e1614f2f5e525c43c6a838f96eacc0e6d31ac633
therealrinku <rinkunited2032@gmail.com>
2026-08-05 21:07:56 +0545

    Update landing page to add note about mp3 and folder

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -29,9 +29,9 @@ export default defineComponent({
     class="bg-[#F88379] text-white min-h-screen min-w-screen text-sm tracking-wide flex flex-col gap-5 items-center justify-center w-full min-h-screen"
   >
     <p>Welcome to rowan music</p>
-    <p>Select a folder to get started</p>
     <button @click="selectFolder" class="bg-green-500 px-5 py-2 rounded-md">
-      Select
+      Select Folder
     </button>
+    <p class="text-xs text-red-100">currently only mp3 is supported, select a folder that has mp3 files.</p>
   </main>
 </template>
