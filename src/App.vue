05e6566781a3b1ac2b1a818e03db5d2ae3e17a1a
therealrinku <rinkunited2032@gmail.com>
2026-08-06 23:07:05 +0545

    Update active song style on right sidebar

--- src/App.vue
+++ src/App.vue
@@ -236,7 +236,7 @@ export default defineComponent({
         <button
           v-for="(song, index) in filteredSongs"
           class="border-b border-[#F88379] w-full py-3 pl-3 flex cursor-pointer"
-          :class="{ 'bg-green-700' : selectedIndex === index }"
+          :class="{ 'bg-[#F88379] font-bold' : selectedIndex === index }"
           @click="playSong(song, index)"
         >
           ♫
